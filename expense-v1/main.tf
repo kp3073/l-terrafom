ae3fe20c30a16c24286244fc7ce2a24169edf7e4
keyurpatel <keyurpatel8288@gmail.com>
2023-12-14 11:59:11 -0500

    terraform first k w

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend1" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = data.aws_security_group.sg.id
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "frontend1"
@@ -19,7 +19,7 @@ resource "aws_route53_record" "frontend1" {
 resource "aws_instance" "backend1" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = data.aws_security_group.sg.id
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "backend1"
@@ -37,7 +37,7 @@ resource "aws_route53_record" "backend1" {
 resource "aws_instance" "mysql1" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = data.aws_security_group.sg.id
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "mysql1"
