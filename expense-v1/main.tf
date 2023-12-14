5d87c8d46aea4dd13b223fef8bea3605596429ee
keyurpatel <keyurpatel8288@gmail.com>
2023-12-13 20:01:11 -0500

    terraform first

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -10,8 +10,44 @@ resource "aws_instance" "frontend1" {
 
 resource "aws_route53_record" "frontend" {
   zone_id = "Z01410302C88NUPGSJO5X"
-  name    = "frontend1.example.com"
+  name    = "frontend1.aligntune.com"
   type    = "A"
-  ttl     = 300
+  ttl     = 30
   records = [aws_instance.frontend1.private_ip]
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0fe6f5f4a7cf08436"]
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z01410302C88NUPGSJO5X"
+  name    = "backend.aligntune.com"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0fe6f5f4a7cf08436"]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z01410302C88NUPGSJO5X"
+  name    = "mysql.aligntune.com"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
 }
