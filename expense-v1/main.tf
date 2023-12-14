35dd924c46eaf054ae7968cdba278c5ede816d2b
keyurpatel <keyurpatel8288@gmail.com>
2023-12-14 13:02:28 -0500

    terraform first k w

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -6,7 +6,16 @@ resource "aws_instance" "frontend1" {
   tags = {
     Name = "frontend1"
   }
+provisioner "local-exec" {
+  command = <<EOF
+cd /root/first-ansible
+git pull
+sleep 90
+ ansible-playbook -i ${self.private_ip}, -e ansible_username=centos -e ansible_passowrd=DevOps321 frontend.yaml
+EOF
 }
+}
+
 
 resource "aws_route53_record" "frontend1" {
   zone_id = data.aws_route53_zone.zone.zone_id
