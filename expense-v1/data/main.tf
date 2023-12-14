90d253b3b8fd83d4de2c235e5c2630f44f8422b4
keyurpatel <keyurpatel8288@gmail.com>
2023-12-13 22:22:35 -0500

    terraform first k

--- expense-v1/data/main.tf
+++ expense-v1/data/main.tf
@@ -1,4 +1,5 @@
 data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["602535206230"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["602535206230"]
+}
