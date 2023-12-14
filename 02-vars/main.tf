e3ff7c21594b0a395850436c3fb9699c0f9856a6
keyurpatel <keyurpatel8288@gmail.com>
2023-12-13 19:14:28 -0500

    terraform first

---
+++ 02-vars/main.tf
@@ -0,0 +1,18 @@
+variable "fruit" {
+  default = "apple"
+}
+
+variable "fruits" {
+  default = [ "apple" , "banana" ]
+}
+
+variable "fruits_price" {
+  default = {
+  apple = 10
+    banana = 20
+  }
+}
+
+output "fruit" {
+  value = "var.fruit"
+}
