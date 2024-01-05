b83beb6f743b2e68ed75e24df848f6bea6fc5d58
keyurpatel <keyurpatel8288@gmail.com>
2024-01-05 15:12:02 -0500

    terraform first k wqw

--- newseries/provider/main.tf
+++ newseries/provider/main.tf
@@ -1,7 +1,15 @@
 provider "github" {}
 
-resource "github_repository" "terraforminit" {
-  name        = "example"
+resource "github_repository" "firstrepo" {
+  name        = "first"
+  description = "My awesome codebase"
+
+  visibility = "public"
+  auto_init = true
+}
+
+resource "github_repository" "secondrepo" {
+  name        = "second"
   description = "My awesome codebase"
 
   visibility = "public"
