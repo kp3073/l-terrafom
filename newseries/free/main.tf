d3329ca2f291d1774ada47ad829bff11e755f854
keyurpatel <keyurpatel8288@gmail.com>
2024-01-05 10:15:41 -0500

    terraform first k w

--- newseries/free/main.tf
+++ newseries/free/main.tf
@@ -1,8 +1,16 @@
 variable "names" {
   type = list(string)
-  default = ["rohan","oreo","vrunda"]
+  default = ["rohan","oreo","vrUNda"]
+}
+
+output "second" {
+  value = "first name is ${title(var.names[1])}"
 }
 
 output "firstname" {
-  value = "first name is ${title(var.names[1])}"
+  value = "first name is ${upper(var.names[0])}"
+}
+
+output "third" {
+  value = "first name is ${lower(var.names[1])}"
 }
