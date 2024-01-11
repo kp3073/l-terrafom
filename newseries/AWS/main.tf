11177b4bba0dceeeb8dd9abc61e556f061ab6823
keyurpatel <keyurpatel8288@gmail.com>
2024-01-10 20:13:52 -0500

    terraform first k wqw

--- newseries/AWS/main.tf
+++ newseries/AWS/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAYYSPFTVLB2LSGO63"
+  secret_key = "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"
+}
 resource "aws_key_pair" "id_rsa" {
   key_name   = "id_rsa"
   public_key = "file(${path.module}/id_rsa)"
