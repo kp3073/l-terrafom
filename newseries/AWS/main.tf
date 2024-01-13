87f62dd491aeaa936309e3a24e801d883a7fbac3
keyurpatel <keyurpatel8288@gmail.com>
2024-01-13 15:29:49 -0500

    terraform first k wqwj

--- newseries/AWS/main.tf
+++ newseries/AWS/main.tf
@@ -1,11 +1,11 @@
-resource "aws_key_pair" "id_rsa" {
-  key_name   = "id_rsa"
-  public_key = file("${path.module}/id_rsa.pub")
+resource "aws_key_pair" "id_key" {
+  key_name   = "id_key"
+  public_key = file("${path.module}/id.key.pub")
 }
 
 resource "aws_instance" "devops" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.id_rsa.key_name
+  key_name = aws_key_pair.id_key.key_name
 }
 
