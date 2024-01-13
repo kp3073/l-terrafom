77ad4532c7cfe041b77f0559d70927a1841625b9
keyurpatel <keyurpatel8288@gmail.com>
2024-01-13 16:43:29 -0500

    terraform first k wqwj

--- newseries/AWS/main.tf
+++ newseries/AWS/main.tf
@@ -10,17 +10,15 @@ resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = "[22,443,8080,3306,27017]"
+    iterator = new
+    content {
+      from_port   = new.value
+      to_port     = new.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 }
 
