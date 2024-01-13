3b42522081fa4921480b9594a646323c48908cf5
keyurpatel <keyurpatel8288@gmail.com>
2024-01-13 16:11:21 -0500

    terraform first k wqwj

--- newseries/AWS/main.tf
+++ newseries/AWS/main.tf
@@ -9,6 +9,7 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id = "vpc-03dbf46cce445afd3 "
 
     ingress {
       from_port   = 443
