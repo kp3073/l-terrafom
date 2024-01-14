9798393f2f9f491bdb6d175dc7746413f3e6b619
keyurpatel <keyurpatel8288@gmail.com>
2024-01-13 19:16:19 -0500

    terraform first k wqwj

--- newseries/AWS/main.tf
+++ newseries/AWS/main.tf
@@ -1,33 +1,33 @@
-### creating ssh-key
-#
+#### creating ssh-key
+##
 resource "aws_key_pair" "id_key" {
   key_name   = "id_key"
   public_key = file("${path.module}/id.key.pub")
 }
-
-## creating security group
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-
-  dynamic "ingress" {
-    for_each = [22,443,8080,3306,27017]
-    iterator = new
-    content {
-      from_port   = new.value
-      to_port     = new.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-}
+#
+### creating security group
+#resource "aws_security_group" "allow_tls" {
+#  name        = "allow_tls"
+#  description = "Allow TLS inbound traffic and all outbound traffic"
+#
+#  dynamic "ingress" {
+#    for_each = [22,443,8080,3306,27017]
+#    iterator = new
+#    content {
+#      from_port   = new.value
+#      to_port     = new.value
+#      protocol    = "tcp"
+#      cidr_blocks = ["0.0.0.0/0"]
+#    }
+#  }
+#}
 
 ### creating EC2
 
 resource "aws_instance" "new" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
-  security_groups = ["${aws_security_group.allow_tls.id}"]
+#security_groups = ["${aws_security_group.allow_tls.id}"]
   key_name = aws_key_pair.id_key.key_name
 }
 
