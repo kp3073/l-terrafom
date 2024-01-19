a0d1918abb058c134cf342cb35104cf7970edb55
keyurpatel <keyurpatel8288@gmail.com>
2024-01-19 13:45:40 -0500

    New change

--- newseries/AWS/instance.tf
+++ newseries/AWS/instance.tf
@@ -6,6 +6,9 @@ resource "aws_instance" "keyur" {
   instance_type          = var.machine_type
   vpc_security_group_ids = ["${aws_security_group.allow_pots.id}"]
   key_name               = aws_key_pair.key.key_name
+  tags = {
+    description = "TEST"
+  }
   user_data = <<-EOF
   #!/bin/bash
   sudo dnf install nginx -y
