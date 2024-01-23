08702241d2da59ea0382d291cb11b2614bffe011
keyurpatel <keyurpatel8288@gmail.com>
2024-01-23 11:09:24 -0500

    New change

--- newseries/s3backend/main.tf
+++ newseries/s3backend/main.tf
@@ -7,12 +7,12 @@ terraform {
 }
 
 resource "aws_instance" "keyur" {
-  ami           = "${data.aws_ami.image_id.id}"
+  ami           = "${data.aws_ami.centos.id}"
   instance_type = var.instance_type
 }
 
 
-data "aws_ami" "image_id" {
+data "aws_ami" "centos" {
   owners = [973714476881]
   filter {
     name   = "name"
