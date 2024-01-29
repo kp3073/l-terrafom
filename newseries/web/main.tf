9c15b4f35d5c218764c28b364da627ae0e7712ff
keyurpatel <keyurpatel8288@gmail.com>
2024-01-28 22:07:46 -0500

    New changek

--- newseries/web/main.tf
+++ newseries/web/main.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "terraformaligntunes3"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#resource "aws_s3_bucket" "example" {
+#  bucket = "terraformaligntunes3"
+#
+#  tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#  }
+#}
