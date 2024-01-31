e83d81efe3a72f2eb3adfc07331c20c1af643edb
keyurpatel <keyurpatel8288@gmail.com>
2024-01-31 11:55:36 -0500

    New changek

--- newseries/web/main.tf
+++ newseries/web/main.tf
@@ -44,23 +44,11 @@ resource "aws_s3_object" "index" {
 }
 
 
-resource "aws_s3_object" "error" {
-  bucket =  aws_s3_bucket.mybucket.id
-  key    = "error.html"
-  source = "error.html"
-  acl = "public-read"
-  content_type = "text/html"
-}
-
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.mybucket.id
 
   index_document {
     suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
   }
 
   depends_on = [aws_s3_bucket_acl.mybucket]
