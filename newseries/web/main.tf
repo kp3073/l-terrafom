16629474bc3d7e77fbde5dc17d991a7162ca9016
keyurpatel <keyurpatel8288@gmail.com>
2024-01-31 11:41:42 -0500

    New changek

--- newseries/web/main.tf
+++ newseries/web/main.tf
@@ -33,3 +33,22 @@ resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.mybucket.id
   acl    = "public-read"
 }
+
+resource "aws_s3_object" "index" {
+  bucket =  aws_s3_bucket.mybucket.id
+  key    = "index.html"
+  source = "index.html"
+  acl = "public-read"
+  content_type = "text/html"
+
+}
+
+
+resource "aws_s3_object" "error" {
+  bucket =  aws_s3_bucket.mybucket.id
+  key    = "error.html"
+  source = "error.html"
+  acl = "public-read"
+  content_type = "text/html"
+
+}
